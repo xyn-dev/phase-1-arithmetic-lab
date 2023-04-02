b842d9dbd486462ce9e07d3beac96f3751cac8b3
Hunter Wade <calebh.wade@gmail.com>
2023-04-01 21:45:15 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+{let num1=31;
 
-// Write your code here
+let num2=2;
+
+let multiply = num1*num2;}
+
+{let num3 = 8
+
+let num4 = 2
+
+let mod = 8/2
+}
+
+{let random = 10
+
+Math.floor(Math.random()*10) + 1;}
+
+let max = (1, 2, 10, 16, 18, 20)
